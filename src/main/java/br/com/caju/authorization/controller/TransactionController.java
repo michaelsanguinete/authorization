e8dcccdd0622e1ff32a090b8bd7f713036692321
Michael Sanguinete <michael.sanguinete@gmail.com>
2023-09-05 09:57:52 -0300

    Adicionando CORS

--- src/main/java/br/com/caju/authorization/controller/TransactionController.java
+++ src/main/java/br/com/caju/authorization/controller/TransactionController.java
@@ -16,6 +16,7 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/transaction")
+@CrossOrigin("authorization-production.up.railway.app")
 public class TransactionController {
 
     private final TransactionService service;
