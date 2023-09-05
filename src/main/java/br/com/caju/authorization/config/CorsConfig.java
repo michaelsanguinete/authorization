87546b11b788ab4f9cf536ab4b2e74151db340cc
Michael Sanguinete <michael.sanguinete@gmail.com>
2023-09-05 09:34:27 -0300

    Adicionando CORS

---
+++ src/main/java/br/com/caju/authorization/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package br.com.caju.authorization.config;
+
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Authorization", "Content-Type")
+                .maxAge(3600); // Define o tempo máximo que o navegador deve armazenar a configuração CORS em cache (em segundos).
+    }
+}
