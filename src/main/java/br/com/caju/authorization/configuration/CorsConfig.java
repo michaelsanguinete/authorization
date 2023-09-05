11774ecb15c0b58020a47dc2e49410768ebe4d47
Michael Sanguinete <michael.sanguinete@gmail.com>
2023-09-05 17:56:15 -0300

    Adicionando CORS

--- src/main/java/br/com/caju/authorization/configuration/CorsConfig.java
+++ src/main/java/br/com/caju/authorization/configuration/CorsConfig.java
@@ -1,24 +1,23 @@
 package br.com.caju.authorization.configuration;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
 
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOriginPatterns("*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().addServersItem(new Server().url("https://authorization-production.up.railway.app"))
+                .components(new Components().addSecuritySchemes("basicScheme",
+                        new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("basic")))
+                .info(new Info().title("CAJU Authorization").version("V0")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
     }
 }
